3ddda8cb9d2df85c2b35650795c864f162dc5ea5
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air-de-Dario.local>
2024-05-01 22:13:16 -0600

    [FIX] paid && nabvar section

--- Frontend/src/components/SectionLogo.jsx
+++ Frontend/src/components/SectionLogo.jsx
@@ -1,15 +1,14 @@
-import ImgBgBalls from "../assets/img/bgBalls.avif";
 import IconLogoSection from "../assets/icons/iconLogoSection.avif";
 import "../assets/style/SectionLogo.css";
 
 function SectionLogo() {
     return ( 
         <>
-            <div className="parallax">
+            <div className="parallax mt-5 d-flex justify-content-center align-items-center">
                 <div className="text-center">
                     <img className="logo img-fluid" src={IconLogoSection} type="image/avif"/>
                 </div>
-            </div>  
+            </div>
         </>
      );
 }
--- Frontend/src/components/SectionPay.jsx
+++ Frontend/src/components/SectionPay.jsx
@@ -6,23 +6,29 @@ import "../assets/style/SectionPay.css";
 function SectionPay() {
     return ( 
         <>
-                <div className="gradientDivGreen"></div>
-                <div className="bgGreen text-center">
-                    <p className="tittleInGradiente text-center">Se Acepta Pago En Efectivo</p>
-                    <img className="img-fluid" src={IconBgGreenIcon} type="image/avif"/>
+            <div className="gradientDivGreen"></div>
+            <div className="parallaxBgGreen bgGreen">
+                <p className="tittleInGradiente text-center">Se Acepta Pago En Efectivo</p>
+                <div className="d-flex justify-content-center align-items-center">
+                    <img className="logoBgGreen" src={IconBgGreenIcon} type="image/avif"/>
                 </div>
-                <div className="gradientDivRed"></div>
-                <div className="bgRed text-center">
-                    <p className="tittleInGradiente text-center">Transferencias Bancarias</p>
-                    <img className="img-fluid" src={IconBgRedIcon} type="image/avif"/>
+            </div>
+            <div className="gradientDivRed"></div>
+            <div className="parallaxBgRed bgRed">
+                <p className="tittleInGradiente text-center">Transferencias Bancarias</p>
+                <div className="d-flex justify-content-center align-items-center">
+                    <img className="img-fluid logoBgGreen" src={IconBgRedIcon} type="image/avif"/>
                 </div>
-                <div className="gradientDivOrange"></div>
-                <div className="bgOrange text-center">
-                    <p className="tittleInGradiente text-center">Más Información</p>
-                    <p className="subTittleGrandiente text-center">+52 961 122 6217</p>
-                    <img className="img-fluid" src={IconBgOrangeIcon} type="image/avif"/>
+            </div>
+            <div className="gradientDivOrange"></div>
+            <div className="bgOrange text-center">
+                <p className="tittleInGradiente text-center">Más Información</p>
+                <p className="subTittleGrandiente text-center">+52 961 122 6217</p>
+                <div className="d-flex justify-content-center align-items-center">
+                    <img className="logoBgGreen img-fluid" src={IconBgOrangeIcon} type="image/avif"/>
                 </div>
-                <div className="gradientDivWhite"></div>
+            </div>
+            <div className="gradientDivWhite"></div>
         </>
      );
 }
