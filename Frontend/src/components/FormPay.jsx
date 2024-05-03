6b5bbb79ec148174b0b1ec51823d4d2e07c4716c
Darío Antonio Gutiérrez Álvarez <105560538+Darioantonio20@users.noreply.github.com>
2024-05-03 16:31:51 -0600

    [FIX] Debit Card

--- Frontend/src/components/FormPay.jsx
+++ Frontend/src/components/FormPay.jsx
@@ -4,10 +4,9 @@ function FormPay() {
     return ( 
         <>
         <div className="containerFather">
-            <h1 className="text-light">Se aceptan transerencias bancarias</h1>
+            <h1 className="text-light text-center">Se aceptan transerencias bancarias</h1>
             <div className="card">
                 <div className="card__info">
-                    <div className="card__logo">Visa HSBC</div>
                     <div className="card__chip">
                         <svg className="card__chip-lines" role="img" width="20px" height="20px" viewBox="0 0 100 100" aria-label="Chip">
                             <g opacity="0.8">
@@ -24,7 +23,7 @@ function FormPay() {
                         </svg>
                         <div className="card__chip-texture"></div>
                     </div>
-                    <div className="card__type">Debit</div>
+                    <div className="card__type">HSBC<br/> <div className="card__debit text-center">Debit</div></div>
                     <div className="card__number">
                         <span className="card__digit-group">4213</span>
                         <span className="card__digit-group">1661</span>
@@ -34,8 +33,8 @@ function FormPay() {
                     <div className="card__valid-thru" aria-label="Valid thru">Valid<br/>thru</div>
                     <div className="card__exp-date"><time datetime="2038-01">04/25</time></div>
                     <div className="card__name" aria-label="Dee Stroyer">María Guadalupe<br></br>Álvarez Pérez</div>
-                    <div className="card__vendor" role="img" aria-labelledby="card-vendor">
-                    <span id="card-vendor" className="card__vendor-sr">Visa</span>
+                    <div>
+                        <span>Visa</span>
                     </div>
                     <div className="card__texture"></div>
                 </div>
